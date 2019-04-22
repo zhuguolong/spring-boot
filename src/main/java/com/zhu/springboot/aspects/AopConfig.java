4dff6a2350767c360a5e4179cc4ad47d72260410
zhuguolong <zhuguolongi@outlook.com>
2019-04-22 15:44:53 +0800

    快慢指针法查找连表中间节点；AOP切面启动过程

--- src/main/java/com/zhu/springboot/aspects/AopConfig.java
+++ src/main/java/com/zhu/springboot/aspects/AopConfig.java
@@ -1,5 +1,13 @@
 package com.zhu.springboot.aspects;
 
+import org.springframework.aop.aspectj.annotation.AnnotationAwareAspectJAutoProxyCreator;
+import org.springframework.aop.aspectj.autoproxy.AspectJAwareAdvisorAutoProxyCreator;
+import org.springframework.aop.framework.autoproxy.AbstractAutoProxyCreator;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.aop.framework.autoproxy.AbstractAdvisorAutoProxyCreator;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import org.springframework.beans.factory.config.SmartInstantiationAwareBeanPostProcessor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -12,6 +20,27 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @Configuration
 @EnableAspectJAutoProxy
 public class AopConfig {
+
+    /**
+     * 项目启动时，初始化spring容器，通过
+     * {@link EnableAspectJAutoProxy/>注解导入
+     * {@link org.springframework.context.annotation.AspectJAutoProxyRegistrar/>类
+     * AspectJAutoProxyRegistrar.registerBeanDefinitions() -> AopConfigUtils.registerAspectJAnnotationAutoProxyCreatorIfNecessary(registry);
+     * 再调用registerOrEscalateApcAsRequired()方法注册或升级需要的Apc，此时会传入{@link AnnotationAwareAspectJAutoProxyCreator}类型
+     * {@link AnnotationAwareAspectJAutoProxyCreator/>
+     *  -> {@link AspectJAwareAdvisorAutoProxyCreator/>
+     *      -> {@link AbstractAdvisorAutoProxyCreator/>.setBeanFactory().initBeanFactory()
+     *          -> {@link AbstractAutoProxyCreator/>
+     *              implements {@link SmartInstantiationAwareBeanPostProcessor/>, {@link BeanFactoryAware/>
+     * {@link SmartInstantiationAwareBeanPostProcessor/>
+     *      -> {@link InstantiationAwareBeanPostProcessor/>
+     *          -> {@link BeanPostProcessor/>
+     *              有两个方法：postProcessBeforeInitialization -> 允许这个后置处理器在任何bean初始化回调之前给定新bean实列（初始化之前调用）
+     *                        postProcessAfterInitialization ->允许这个后置处理器在任何bean初始化回调之后给定新bean实列（初始化之后调用）
+     * 其中的{@link BeanFactoryAware/>中有方法setBeanFactory()在正常bean属性的群体创建之后、初始化之前回调
+     * @return
+     */
+
     @Bean
     public LogAspects injectLogAspects() {
         return new LogAspects();
---
+++ src/main/java/com/zhu/springboot/utils/slution/IntermediateNodeLinkedList.java
@@ -0,0 +1,51 @@
+package com.zhu.springboot.utils.slution;
+
+import java.util.List;
+
+/**
+ * @author zhugu
+ * @version 1.0
+ * @Date 2019/4/22 15:06
+ * @Description 如何访问链表中间节点
+ *
+ * 对于这个问题，我们首先能够想到的就是先遍历一遍整个的链表，然后计算出链表的长度，进而遍历第二遍找出中间位置的数据。这种方式非常简单。
+ *
+ * 若题目要求只能遍历一次链表，那又当如何解决问题？
+ *
+ * 可以采取建立两个指针，一个指针一次遍历两个节点，另一个节点一次遍历一个节点，当快指针遍历到空节点时，慢指针指向的位置为链表的中间位置，这种解决问题的方法称为快慢指针方法。
+ */
+public class IntermediateNodeLinkedList {
+    /**
+     * 快慢指针方法
+     */
+    public static Object fastSlowPointer(List<Object> originList) {
+        if (originList.size() <= 0) {
+            throw new IllegalArgumentException("list is size illegal");
+        }
+
+        int fastPointer = 0;
+        int slowPointer = 0;
+        for (int i = 0, l = originList.size(); i < l; i++) {
+            if (fastPointer == l || fastPointer == l - 1) {
+                break;
+            }
+            fastPointer += 2;
+            slowPointer++;
+        }
+
+        return originList.get(slowPointer - 1);
+    }
+
+//    public static void main(String[] args) {
+//        List<Object> originList = new LinkedList<>();
+//        originList.add("1");
+//        originList.add("2");
+//        originList.add("3");
+//        originList.add("4");
+//        originList.add("5");
+//        originList.add("6");
+//
+//        Object obg = fastSlowPointer(originList);
+//        System.out.println(obg);
+//    }
+}
